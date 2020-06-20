2ff50dc651bf576776a4f7abfaf1e1c1f7f51afb
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2020-06-20 11:21:33 +0700

    heroku fix bug remove

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -6,6 +6,6 @@ router.get('/get-product', controller.getAllProduct);
 router.get('/get-product/:id', controller.getSpecificProduct);
 router.post('/upload-product', fileUpload(), controller.uploadProduct);
 router.put('/update-product/:id', fileUpload(), controller.updateProduct);
-router.delete('remove-product/:id', controller.removeProduct);
+router.delete('/remove-product/:id', controller.removeProduct);
 
 module.exports = router;
