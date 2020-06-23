58046621afc5f74a804aabd70583d151627d1266
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2020-06-23 13:33:42 +0700

    perbaikan

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,13 +8,13 @@ exports.registerAdmin = async (req, res, next) => {
     const data = await Users.findOne({ role: "admin" });
     if (!data) {
       const newAdmin = new Users({
-        role: "admin",
+        role: 'admin',
         username: "admin",
       });
       const hash = await bcrypt.hash("admin", 10);
       newAdmin.password = hash;
       const savedAdmin = await newAdmin.save();
-      return res.json({ msg: "Success Create Admin", data: savedAdmin });
+      return res.json({ msg: "Success Create Admin", saved: savedAdmin });
     }
   } catch (e) {
     next(e);
@@ -47,12 +47,64 @@ exports.loginAdmin = async (req, res, next) => {
         return res.json({
           success: true,
           token: token,
-          role: result.role,
         });
       }
       throw new Error("Wrong Password");
     }
     throw new Error("Admin not created yet");
+  } catch (e) {
+    next(e);
+  }
+};
+
+// REGISTER MEMBER
+exports.registerMember = async (req, res, next) => {
+  try {
+    const newUser = new Users({
+      role: 'member',
+      name: req.body.name,
+      username: req.body.username,
+      address: req.body.address
+    });
+    const hash = await bcrypt.hash(req.body.password, 10);
+    newUser.password = hash;
+    const savedMember = await newUser.save();
+    return res.json({ msg: "Success Register", saved: savedMember });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// LOGIN MEMBER
+exports.loginMember = async (req, res, next) => {
+  try {
+    const result = await Users.findOne({
+      username: req.body.username
+    });
+    if (result) {
+      const isMatch = await bcrypt.compare(req.body.password, result.password);
+      if (isMatch) {
+        const payload = {
+          username: result.username
+        };
+        const token = await new Promise((resolve, reject) => {
+          jwt.sign(
+            payload,
+            "nyongpenyi",
+            { expiresIn: "30m" },
+            (err, token) => {
+              if (err) reject(err);
+              resolve(token);
+            }
+          );
+        });
+        return res.json({
+          message: "Happy Shopping",
+          token: token,
+        });
+      }
+    }
+    throw new Error("username or password wrong");
   } catch (e) {
     next(e);
   }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,5 +3,7 @@ const controller = require('../controller/user.controller');
 
 router.post('/register-admin', controller.registerAdmin);
 router.post('/login-admin', controller.loginAdmin);
+router.post('/register-member', controller.registerMember);
+router.post('/login-member', controller.loginMember);
 
 module.exports = router;
